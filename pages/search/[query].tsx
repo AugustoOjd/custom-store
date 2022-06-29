27b60a57604cb5f922c857178bf8c9f729c2f383
Augusto Ojeda <okarinex@gmail.com>
2022-06-28 22:13:09 -0300

    arreglando busqueda

--- pages/search/[query].tsx
+++ pages/search/[query].tsx
@@ -6,12 +6,16 @@ import { Box } from '@mui/system';
 import { ProductList } from '../../components/products';
 import { useProducts } from '../../hooks';
 import { FullScreenLoading } from '../../components/ui';
+import { GetServerSideProps } from 'next';
+import { dbProducts } from '../../database';
+import { IProduct } from '../../interface';
 
+interface Props{
+  products: IProduct[]
+}
 
+const SearchPage: NextPage<Props> = ({products}) => {
 
-const SearchPage: NextPage = () => {
-
-  const { products, isLoading, isError} = useProducts(`/search/${'faltaquery'}`)
 
   return (
     <>
@@ -27,12 +31,9 @@ const SearchPage: NextPage = () => {
             {}
         </Typography>
 
-        {
 
-          isLoading
-          ? <FullScreenLoading/>
-          : <ProductList products={ products }/>
-        }
+        <ProductList products={ products }/>
+
 
       {/* <ProductList products={ initialData.products as any}/> */}
       
@@ -40,6 +41,31 @@ const SearchPage: NextPage = () => {
     </ShopLayout>
     </>
   )
+}
+
+
+export const getServerSideProps: GetServerSideProps = async ({params}) => {
+
+  const { query = '' } = params as {query: string}
+
+  if(query.length === 0){
+    return{
+      redirect: {
+        destination: '/',
+        permanent: true
+      }
+    }
+  }
+
+  let products = await dbProducts.getProductsByTerm( query )
+
+
+  // TODO: alternativas de productos
+  return {
+    props: {
+      products
+    }, // will be passed to the page component as props
+  }
 }
 
 export default SearchPage
